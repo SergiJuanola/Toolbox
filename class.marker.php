16d152d85091ee0677dd69146975589eabd36ce1
Sergi Juanola <korcholis@gmail.com>
2012-12-28 11:28:22 +0100

    added marker, a markdown parser

---
+++ class.marker.php
@@ -0,0 +1,26 @@
+<?php 
+	require_once 'class.builder.php';
+	require_once 'vendor/php-markdown-extra/markdown.php';
+	
+	class Marker extends Builder {
+		public static $default = array();
+
+		public static function build($config = array()) {
+			$config['__parser'] = new MarkdownExtra_Parser();
+			return new self($config);
+		}
+
+		public function transform($text, $return = FALSE)
+		{
+			$marked = $this->__parser->transform($text);
+			if($return === TRUE)
+				return $marked;
+			else
+				echo $marked;
+		}
+
+		public function show($text)
+		{
+			$this->transform($text, FALSE);
+		}
+	}
