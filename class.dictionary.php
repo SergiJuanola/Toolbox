beb4af26d73b88938c0cc4dff33e6178088199e3
Korcholis <korcholis@gmail.com>
2013-01-21 20:19:16 +0100

    changed dictionary to translate, so dictionary is now static texts

--- class.dictionary.php
+++ class.dictionary.php
@@ -5,25 +5,39 @@ require_once 'class.builder.php';
 class Dictionary extends Builder {
 
 	public static $default = array(
-		'locale' => 'en',
-		'orm' => null,
-		'transTableName' => 'text',
-		'dictionarySuffix' => '_ml'
+		'match'=>NULL,
+		'dictionaries'=>NULL,
+		'canTranslate'=>FALSE,
+		'__dictionary' => NULL
 	);
 
 	public static function build($config = array()) {
-		return new self($config);
-	}
-
-	public function translate(&$object) {
-		$suffixLen = strlen($this->dictionarySuffix);
-		foreach ($object as $key => $value) {
-			if(strpos($key, $this->dictionarySuffix)===strlen($key)-$suffixLen)
+		$dict = new self($config);
+		if(empty($dict->dictionaries) || empty($dict->match) || !$dict->match->hasLocale())
+		{
+			$dict->canTranslate = FALSE;
+		}
+		else
+		{
+			$locale = empty($dict->match->locale)? $dict->match->getDefaultLocale() : $dict->match->locale;
+			$file = $dict->dictionaries.$locale.'.php';
+			if(!file_exists($file))
 			{
-				$row = $this->orm->from($this->transTableName)->selectById($value);
-				$text = $row[$this->locale];
-				$object[substr($key, 0,-$suffixLen)] = $text;
+				$dict->canTranslate = FALSE;
+			}
+			else
+			{
+				$dict->__dictionary=include($file);
+				$dict->canTranslate = TRUE;
 			}
 		}
+		return $dict;
+	}
+
+	public function t($sentence)
+	{
+		if($this->canTranslate === TRUE && array_key_exists($sentence, $this->__dictionary))
+			return $this->__dictionary[$sentence];
+		return $sentence;
 	}
 }
---
+++ class.translate.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once 'class.builder.php';
+
+class Translate extends Builder {
+
+	public static $default = array(
+		'locale' => 'en',
+		'orm' => null,
+		'transTableName' => 'text',
+		'translateSuffix' => '_ml'
+	);
+
+	public static function build($config = array()) {
+		return new self($config);
+	}
+
+	public function translate(&$object) {
+		$suffixLen = strlen($this->dictionarySuffix);
+		foreach ($object as $key => $value) {
+			if(strpos($key, $this->dictionarySuffix)===strlen($key)-$suffixLen)
+			{
+				$row = $this->orm->from($this->transTableName)->selectById($value);
+				$text = $row[$this->locale];
+				$object[substr($key, 0,-$suffixLen)] = $text;
+			}
+		}
+	}
+}
